8932299d35c344641dbb8d4dba44df709a7354f2
damanh1905 <18130005@st.hcmuaf.edu.vn>
2022-05-15 13:53:01 +0700

    setupproject

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Fragment } from "react";
 import { publicRouterUser } from "./routers";
 import DefaultLayout from "./components/Layout/DefaultLayout";
 function App() {
@@ -7,7 +8,7 @@ function App() {
       <div>
         <Routes>
           {publicRouterUser.map((router, index) => {
-            const Layout = router.layout || DefaultLayout;
+            const Layout = router.layout === null ? Fragment:  DefaultLayout;
             const Page = router.component;
             return (
               <Route
--- src/routers/index.js
+++ src/routers/index.js
@@ -2,6 +2,6 @@ import Login from "../containers/HomeTemplate/Login";
 import Product from "../containers/HomeTemplate/product/Product";
 const publicRouterUser = [
   { path: "/", component: Login },
-  { path: "/product", component: Product },
+  { path: "/product", component: Product,layout:null },
 ];
 export {publicRouterUser} 

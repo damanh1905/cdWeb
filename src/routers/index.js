957d6c1267f0d73946ec3b6c5d096040067b1586
hiệp hoàng <18130077@st.hcmuaf.edu.vn>
2022-05-28 08:31:55 +0700

    a

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,7 +1,9 @@
 import Login from "../containers/HomeTemplate/Login";
+import BlogDetail from "../containers/HomeTemplate/BlogDetail";
 import Product from "../containers/HomeTemplate/product/Product";
 const publicRouterUser = [
   { path: "/", component: Login },
+  { path: "/blogDetail", component: BlogDetail },
   { path: "/product", component: Product,layout:null },
 ];
 export {publicRouterUser} 

e29ba18a34d3b97b7bc2f69f5efe8cd71e94371b
hiệp hoàng <18130077@st.hcmuaf.edu.vn>
2022-05-28 13:50:30 +0700

    convert

--- src/routers/index.js
+++ src/routers/index.js
@@ -2,10 +2,22 @@ import Login from "../containers/HomeTemplate/Login";
 import BlogDetail from "../containers/HomeTemplate/BlogDetail";
 import Product from "../containers/HomeTemplate/product/Product";
 import Blog from "../containers/HomeTemplate/Blog";
+import CheckOut from "../containers/HomeTemplate/CheckOut";
+import Contact from "../containers/HomeTemplate/Contact";
+import Home from "../containers/HomeTemplate/Home";
+import ShopDetail from "../containers/HomeTemplate/ShopDetail";
+import ShopGrid from "../containers/HomeTemplate/ShopGrid";
+import ShoppingCart from "../containers/HomeTemplate/ShoppingCart";
 const publicRouterUser = [
-  { path: "/", component: Login },
+  { path: "/", component: Home },
   { path: "/blogDetail", component: BlogDetail },
   { path: "/blog", component: Blog },
+  { path: "/checkout", component: CheckOut },
+  { path: "/contact", component: Contact },
+  { path: "/home", component: Home },
+  { path: "/shopDetail", component: ShopDetail },
+  { path: "/shopGrid", component: ShopGrid },
+  { path: "/shoppingCart", component: ShoppingCart },
   { path: "/product", component: Product,layout:null },
 ];
 export {publicRouterUser} 

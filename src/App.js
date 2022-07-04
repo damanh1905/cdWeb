a894f76da4f9bf586133dd6b44b6feda09760d51
Hiep299 <85495849+Hiep299@users.noreply.github.com>
2022-07-04 11:07:53 +0700

    update scroll

--- src/App.js
+++ src/App.js
@@ -2,7 +2,27 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Fragment } from "react";
 import { publicRouterUser } from "./routers";
 import DefaultLayout from "./components/Layout/DefaultLayout";
+import React, { useState,useEffect } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 function App() {
+  const [scrollTop,setScrollTop]=useState(false);
+  console.log(scrollTop)
+  const handleScroll=()=>{
+    if(window.scrollY>500){
+        setScrollTop(!scrollTop)
+    }else{
+      setScrollTop(false)
+    }
+}
+  useEffect(()=>{
+    window.addEventListener('scroll',handleScroll)
+  },[])
+  const goToTop=()=>{
+    window.scrollTo({
+      top: 0, 
+      behavior: 'smooth'
+    });
+  }
   return (
     
     <Router>
@@ -23,6 +43,7 @@ function App() {
             );
           })}
           </Routes>
+          { scrollTop&&<button style={{position:"fixed",bottom:20,right:20,padding:7,paddingLeft:14,cursor:'pointer',backgroundColor:'#1890ff',color:'white',border:"none"}} onClick={goToTop}><i class="fa-solid fa-arrow-up-long"></i></button>}
     </Router>
  
   );
